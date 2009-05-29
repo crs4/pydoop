7402afe2bd591cb1e84003f0719d813795aeed8d
zag <zag@fe8e3fcf-6784-4a38-948d-26a2c6abf5d7>
2009-05-29 10:30:13 +0000

    hdfs_experiment: back in sync
    git-svn-id: https://version.crs4.it/svn/ac-dc/lib/pydoop/trunk@965 fe8e3fcf-6784-4a38-948d-26a2c6abf5d7

--- examples/hdfs_experiment/do_get.sh
+++ examples/hdfs_experiment/do_get.sh
@@ -6,7 +6,7 @@ h=`hostname`
 trg=/scratch/${img_name}.${h}
 hdp="/ELS/els5/acdc/opt/hadoop-0.19.1/bin/hadoop --config ${conf_dir}"
 ${hdp} fs -get ${img_name} ${trg}
-ls -l ${trg}
+#ls -l ${trg}
 
 
 
--- examples/hdfs_experiment/do_get_specific.sh
+++ examples/hdfs_experiment/do_get_specific.sh
@@ -7,7 +7,7 @@ img_name_specific=${img_name}.${h}
 trg=/scratch/${img_name_specific}.back
 hdp="/ELS/els5/acdc/opt/hadoop-0.19.1/bin/hadoop --config ${conf_dir}"
 ${hdp} fs -get ${img_name_specific} ${trg}
-ls -l ${trg}
+#ls -l ${trg}
 
 
 
