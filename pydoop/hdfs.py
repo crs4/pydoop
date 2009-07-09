974e72f83b6431861aae722ab532fe6c8e804150
zag <zag@fe8e3fcf-6784-4a38-948d-26a2c6abf5d7>
2009-07-09 12:49:49 +0000

    lib::pydoop: minor refactoring
    git-svn-id: https://version.crs4.it/svn/ac-dc/lib/pydoop/trunk@1122 fe8e3fcf-6784-4a38-948d-26a2c6abf5d7

--- pydoop/hdfs.py
+++ pydoop/hdfs.py
@@ -1,22 +1,3 @@
+from pydoop_hdfs import hdfs_fs  as hdfs
 
-from pydoop_hdfs import hdfs_fs as hdfs
 
-#-------------------------------------
-# utility functions
-
-DEFAULT_HDFS_PORT=9000
-
-import os
-def split_hdfs_path(path):
-  root = ''
-  tail = ''
-  while path.find(os.path.sep) > -1:
-    root = os.path.join(tail, root)
-    path, tail = os.path.split(path)
-  assert path == 'hdfs:' and len(tail) > 0
-  root = os.path.sep + root[:-1]
-  parts = tail.split(':')
-  if len(parts) == 1:
-    return parts[0], DEFAULT_HDFS_PORT, root
-  else:
-    return parts[0], int(parts[1]), root
---
+++ pydoop/utils.py
@@ -0,0 +1,33 @@
+#-------------------------------------
+# utility functions
+
+DEFAULT_HDFS_PORT=9000
+
+def jc_configure(obj, jc, k, f, df):
+  v = df
+  if jc.hasKey(k):
+    v = jc.get(k)
+  setattr(obj, f, v)
+#--
+def jc_configure_int(obj, jc, k, f, df):
+  v = df
+  if jc.hasKey(k):
+    v = jc.getInt(k)
+  setattr(obj, f, v)
+
+#--
+import os
+def split_hdfs_path(path):
+  root = ''
+  tail = ''
+  while path.find(os.path.sep) > -1:
+    root = os.path.join(tail, root)
+    path, tail = os.path.split(path)
+  assert path == 'hdfs:' and len(tail) > 0
+  root = os.path.sep + root[:-1]
+  parts = tail.split(':')
+  if len(parts) == 1:
+    return parts[0], DEFAULT_HDFS_PORT, root
+  else:
+    return parts[0], int(parts[1]), root
+
