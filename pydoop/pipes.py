beb4f0b719e8a19ce5b9e4932b479ee181ca115f
zag <zag@fe8e3fcf-6784-4a38-948d-26a2c6abf5d7>
2009-08-08 23:16:18 +0000

    lib::pydoop::pydoop clean up.
    git-svn-id: https://version.crs4.it/svn/ac-dc/lib/pydoop/trunk@1274 fe8e3fcf-6784-4a38-948d-26a2c6abf5d7

--- pydoop/pipes.py
+++ pydoop/pipes.py
@@ -3,7 +3,7 @@ from pydoop_pipes import Mapper  as Mapper
 from pydoop_pipes import Reducer as Reducer
 from pydoop_pipes import RecordReader as RecordReader
 from pydoop_pipes import RecordWriter as RecordWriter
-#from pydoop_pipes import Combiner     as Combiner
+from pydoop_pipes import Reducer      as Combiner
 from pydoop_pipes import Partitioner  as Partitioner
 
 from factory      import Factory
