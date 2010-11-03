8aa94a526502c6ee4a53a5162007401cc64f8856
simleo <simleo@fe8e3fcf-6784-4a38-948d-26a2c6abf5d7>
2010-11-03 10:51:11 +0000

    lib/pydoop: added test for ipcount
    git-svn-id: https://version.crs4.it/svn/ac-dc/lib/pydoop/trunk@2748 fe8e3fcf-6784-4a38-948d-26a2c6abf5d7

---
+++ examples/ipcount/test_ipcount.py
@@ -0,0 +1,31 @@
+import sys, subprocess, unittest
+
+
+class TestIPCount(unittest.TestCase):
+
+  def runTest(self):
+    args = ["./ipcount", "-e", "excludes.txt", "input"]
+    expected_out = [
+      ["156.148.66.36", "682"],
+      ["156.148.71.251", "566"],
+      ["156.148.71.159", "154"],
+      ["156.148.71.202", "153"],
+      ["156.148.68.115", "122"],
+      ]
+    sys.stderr.write("\n")
+    sys.stderr.flush()
+    p = subprocess.Popen(args, stdout=subprocess.PIPE)
+    out = p.communicate()[0].splitlines()
+    out = [l.split() for l in out]
+    self.assertEqual(out, expected_out)
+
+
+def suite():
+  suite = unittest.TestSuite()
+  suite.addTest(TestIPCount("runTest"))
+  return suite
+
+
+if __name__ == '__main__':
+  runner = unittest.TextTestRunner(verbosity=2)
+  runner.run((suite()))
