e6d211915b690b167afa19bd8bdfcf720de5942a
simleo <simleo@fe8e3fcf-6784-4a38-948d-26a2c6abf5d7>
2010-11-10 11:16:50 +0000

    lib/pydoop/examples/ipcount: added support for Hadoop 0.21.0
    git-svn-id: https://version.crs4.it/svn/ac-dc/lib/pydoop/trunk@2765 fe8e3fcf-6784-4a38-948d-26a2c6abf5d7

--- examples/ipcount/test_ipcount.py
+++ examples/ipcount/test_ipcount.py
@@ -1,13 +1,15 @@
 # BEGIN_COPYRIGHT
 # END_COPYRIGHT
 
-import sys, subprocess, unittest
+import sys, os, subprocess, unittest, tempfile, shutil
 
 
 class TestIPCount(unittest.TestCase):
 
   def runTest(self):
-    args = ["./ipcount", "-e", "excludes.txt", "input"]
+    temp_dir = tempfile.mkdtemp(prefix="pydoop_test_ipcount_")
+    outfn = os.path.join(temp_dir, "ipcount.out")
+    args = ["./ipcount", "-e", "excludes.txt", "input", "--local-output", outfn]
     expected_out = [
       ["156.148.66.36", "682"],
       ["156.148.71.251", "566"],
@@ -17,10 +19,12 @@ class TestIPCount(unittest.TestCase):
       ]
     sys.stderr.write("\n")
     sys.stderr.flush()
-    p = subprocess.Popen(args, stdout=subprocess.PIPE)
-    out = p.communicate()[0].splitlines()
-    out = [l.split() for l in out]
+    p = subprocess.check_call(args)
+    outf = open(outfn)
+    out = [l.split() for l in outf]
+    outf.close()
     self.assertEqual(out, expected_out)
+    shutil.rmtree(temp_dir)
 
 
 def suite():
