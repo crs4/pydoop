d1dd7e3e4bfa0b053c9d7e0084ec2e1c8c414641
Luca Pireddu <pireddu@crs4.it>
2012-02-10 12:31:11 +0100

    Removed Seal left-overs from hadut module.

--- pydoop/hadut.py
+++ pydoop/hadut.py
@@ -1,29 +1,10 @@
 
-# Copyright (C) 2011-2012 CRS4.
-#
-# This file is part of Seal.
-#
-# Seal is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option)
-# any later version.
-#
-# Seal is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with Seal.  If not, see <http://www.gnu.org/licenses/>.
-
+# BEGIN_COPYRIGHT
+# END_COPYRIGHT
 
 import copy
 import os
-import sys
 import subprocess
-import glob
-
-SealJarName = "seal.jar"
 
 def __is_exe(fpath):
 	return os.path.exists(fpath) and os.access(fpath, os.X_OK)
