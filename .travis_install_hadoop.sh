e4c64a6ddb48631f520fee04207101128348bc6b
Mauro Del Rio <mauro@crs4.it>
2014-05-28 12:28:05 +0200

    modified travis

--- .travis_install_hadoop.sh
+++ .travis_install_hadoop.sh
@@ -62,6 +62,8 @@ if [[ "$1" != *cdh* ]]; #standard hadoop distribution
         
         for i in `cd /etc/init.d; ls hadoop*`; do sudo service $i start; done   
         hadoop dfsadmin -safemode wait;
+        sudo service hadoop-0.20-mapreduce-jobtracker start;
+        sudo service hadoop-0.20-mapreduce-tasktracker start;
         sudo jps
         cat /var/log/hadoop-0.20-mapreduce/hadoop-hadoop-jobtracker-*.log
         hdfs="sudo -u hdfs hadoop fs"; 
