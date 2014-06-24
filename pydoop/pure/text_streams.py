b6eef48a79ffd218a442d3d9a5dba43df2075fed
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2014-06-24 23:12:12 +0200

    Added code to text_streams.
    Missed in previous commit.

--- pydoop/pure/text_streams.py
+++ pydoop/pure/text_streams.py
@@ -1,6 +1,11 @@
 from streams import DownStreamFilter, UpStreamFilter
 from string_utils import quote_string, unquote_string
 
+def toBool(s):
+    return s.lower().find('true') > -1
+
+
+
 class TextDownStreamFilter(DownStreamFilter):
     """
     Naive textual stream filter implementation.
@@ -11,6 +16,32 @@ class TextDownStreamFilter(DownStreamFilter):
     *NOTE:* this stream filter is intended for debugging purposes only.
     """
     SEP   = '\t'
+
+    CMD_TABLE = {'mapItem'     : ('mapItem', 2, None),
+                 'reduceValue' : ('reduceValue', 1, None),
+                 'reduceKey'   : ('reduceKey', 1, None),
+                 'start'       : ('start', 1, lambda p: [int(p[0]),]),
+                 'setJobConf'  : ('setJobConf', None, None),
+                 'setInputTypes': ('setInputTypes', 2, None),
+                 'runMap' : ('runMap', 3,
+                             lambda p: [p[0], int(p[1]), toBool(p[2])]),
+                 'runReduce' : ('runReduce', 2,
+                                lambda p: [int(p[0]), toBool(p[1])]),
+                 'abort' : ('abort', 0, None),
+                 'close' : ('close', 0, None),
+                 }
+
+    @classmethod
+    def convert_message(cls, cmd, args):
+        if cmd in cls.CMD_TABLE:
+            cmd, nargs, converter = cls.CMD_TABLE[cmd]
+            assert nargs is None or len(args) == nargs
+            if cmd == 'abort':
+                raise ProtocolAbort('received an abort request')
+            args = args if converter is None else converter(args)
+            return cmd, tuple(args) if args else None
+        else:
+            raise ProtocolError('Unrecognized command %s' % cmd)
 
     def __init__(self, stream):
         super(TextDownStreamFilter, self).__init__(stream)
