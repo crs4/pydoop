78df484021ff910c5ff4f6da4261d1435742bb1c
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2014-09-23 23:00:15 +0200

    Bug fixes in basic avro support.
    Fixed a bug in the sync_marker alignment logic.

--- pydoop/avrolib.py
+++ pydoop/avrolib.py
@@ -7,22 +7,31 @@ from avro.io import DatumReader, DatumWriter
 class SeekableDataFileReader(DataFileReader):
     FORWARD_WINDOW_SIZE = 8192
     def align_after(self, offset):
-        if offset <= 0: # FIXME Negative offsets??
+        "Search for a sync point after offset and align just after that."
+        f = self.reader
+        if offset <= 0: # FIXME what is a negative offset??
+            f.seek(0)
+            self.block_count = 0
+            self._read_header() # FIXME we can't extimate how big it is...
             return
-        f = self.reader.reader
-        sm = self.reader.sync_marker
-        pos = offset + len(sm)
-        while pos < self.file_length:
-            pos -=  len(sm)
+        sm = self.sync_marker 
+        sml = len(sm)
+        pos = offset
+        while pos < self.file_length - sml:
             f.seek(pos)
             data = f.read(self.FORWARD_WINDOW_SIZE)
             sync_offset = data.find(sm)
             if sync_offset > -1:
                 f.seek(pos + sync_offset)
+                self.block_count = 0
                 return
             pos += len(data)
 
+#FIXME this is just an example with no error checking
 class AvroReader(RecordReader):
+    """Read an avro data file. 
+    It will read by record, all the data blocks that begin within the given isplit.
+    """
     def __init__(self, ctx):
         isplit = ctx.input_split
         self.region_start = isplit.offset
@@ -33,23 +42,25 @@ class AvroReader(RecordReader):
 
     def next(self):
         pos = self.reader.reader.tell()
-        if pos > self.region_end:
+        if pos > self.region_end and self.reader.block_count == 0:
             raise StopIteration
         record = self.reader.next()
         return pos, record
 
     def get_progress(self):
+        "Rough estimate of the progress done."
         pos = self.reader.reader.tell()
         return min((pos - self.region_start)
                    / float(self.region_end - self.region_start),
                    1.0)
 
+#FIXME this is just an example with no error checking
 class AvroWriter(RecordWriter):
     schema = None
     def __init__(self, context):
         job_conf = context.job_conf
-        part   = int(job_conf['mapred.task.partition'])
-        outdir = job_conf["mapred.work.output.dir"]
+        part   = int(job_conf['mapreduce.task.partition'])
+        outdir = job_conf["mapreduce.task.output.dir"]
         outfn = "%s/part-%05d" % (outdir, part)
         self.writer = DataFileWriter(hdfs.open(outfn, "w"),
                                      DatumWriter(), self.schema)
