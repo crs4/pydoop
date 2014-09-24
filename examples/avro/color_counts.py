c3e3e9a885141d54bf7c07fba6f5643bb1f46995
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2014-09-24 08:32:52 +0200

    Added explicit serialization to avro color_counts example.
    This is a temporary workaround until we decide how do we want to implement
    mapper-reducer data exchange magic.

--- examples/avro/color_counts.py
+++ examples/avro/color_counts.py
@@ -13,8 +13,14 @@ from pydoop.avrolib import AvroReader, AvroWriter
 import avro.schema
 
 from collections import Counter
+import cPickle
+import itertools as it
 
-import sys
+def serialize(x):
+    return cPickle.dumps(x, cPickle.HIGHEST_PROTOCOL)
+
+def deserialize(x):
+    return cPickle.loads(x)
 
 class UserReader(AvroReader):
     pass
@@ -22,24 +28,19 @@ class UserReader(AvroReader):
 class ColorWriter(AvroWriter):
     schema = avro.schema.parse(open("stats.avsc").read())
     def emit(self, key, value):
-        sys.stderr.write('ready to write {} {}\n'.format(key, value))
-        self.writer.append({'office': key, 'counts': value})
-        f = self.writer.writer
-        f.flush()
-        sys.stderr.write('name: {} fs: {} size: {} tell: {}\n'.format(
-            f.name, f.fs, f.size, f.tell()))
+        self.writer.append({'office': key, 'counts': deserialize(value)})
         
 class ColorPick(api.Mapper):
     def map(self, ctx):
         user = ctx.value
         color = user['favorite_color']
         if color is not None:
-            ctx.emit(user['office'], Counter({color : 1}))
+            ctx.emit(user['office'], serialize(Counter({color : 1})))
 
 class ColorCount(api.Reducer):
     def reduce(self, ctx):
-        s = sum(ctx.values, Counter())
-        ctx.emit(ctx.key, s)
+        s = sum(it.imap(deserialize, ctx.values), Counter())
+        ctx.emit(ctx.key, serialize(s))
 
 pp.run_task(pp.Factory(mapper_class=ColorPick,
                        reducer_class=ColorCount,
