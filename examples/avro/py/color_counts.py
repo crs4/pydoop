c988f0fdf18463ab77bae39cfecfa6e5f0e2f7f9
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2014-10-06 19:01:32 +0200

    Upgraded color_counts example to use private_encoding.

--- examples/avro/py/color_counts.py
+++ examples/avro/py/color_counts.py
@@ -13,14 +13,8 @@ from pydoop.avrolib import AvroReader, AvroWriter
 import avro.schema
 
 from collections import Counter
-import cPickle
+
 import itertools as it
-
-def serialize(x):
-    return cPickle.dumps(x, cPickle.HIGHEST_PROTOCOL)
-
-def deserialize(x):
-    return cPickle.loads(x)
 
 class UserReader(AvroReader):
     pass
@@ -28,21 +22,23 @@ class UserReader(AvroReader):
 class ColorWriter(AvroWriter):
     schema = avro.schema.parse(open("stats.avsc").read())
     def emit(self, key, value):
-        self.writer.append({'office': key, 'counts': deserialize(value)})
+        self.writer.append({'office': key, 'counts': value})
         
 class ColorPick(api.Mapper):
     def map(self, ctx):
         user = ctx.value
         color = user['favorite_color']
         if color is not None:
-            ctx.emit(user['office'], serialize(Counter({color : 1})))
+            ctx.emit(user['office'], Counter({color : 1}))
 
 class ColorCount(api.Reducer):
     def reduce(self, ctx):
-        s = sum(it.imap(deserialize, ctx.values), Counter())
-        ctx.emit(ctx.key, serialize(s))
+        s = sum(ctx.values, Counter())
+        ctx.emit(ctx.key, s)
 
 pp.run_task(pp.Factory(mapper_class=ColorPick,
                        reducer_class=ColorCount,
                        record_reader_class=UserReader,
-                       record_writer_class=ColorWriter))
+                       record_writer_class=ColorWriter),
+            private_encoding=True
+)
