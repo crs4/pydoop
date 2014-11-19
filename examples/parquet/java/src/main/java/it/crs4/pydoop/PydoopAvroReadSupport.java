e16560cd38c8d156e4e17e0f218d093523557dd3
simleo <simleo@crs4.it>
2014-11-19 16:49:08 +0100

    added missing copyright notice to examples

--- examples/parquet/java/src/main/java/it/crs4/pydoop/PydoopAvroReadSupport.java
+++ examples/parquet/java/src/main/java/it/crs4/pydoop/PydoopAvroReadSupport.java
@@ -1,5 +1,25 @@
+/* BEGIN_COPYRIGHT
+ *
+ * Copyright 2009-2014 CRS4.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * END_COPYRIGHT
+ */
+
 /*
-  Reads an AvroParquet file and returns raw, binary encoded, serialized avro objects.
+ * Reads an AvroParquet file and returns raw, binary encoded,
+ * serialized avro objects.
  */
 
 package it.crs4.pydoop;
