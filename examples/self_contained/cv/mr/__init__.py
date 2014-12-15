f36afbcfaf9c60dc16734d788d2c4bc27a87e7b6
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2014-12-16 00:24:29 +0100

    Updated self_contained example.
    Now it uses pydoop submit and wheel packaging.

--- examples/self_contained/cv/mr/__init__.py
+++ examples/self_contained/cv/mr/__init__.py
@@ -16,10 +16,3 @@
 #
 # END_COPYRIGHT
 
-from pydoop.pipes import runTask, Factory
-from mapper import Mapper
-from reducer import Reducer
-
-
-def run_task():
-    return runTask(Factory(Mapper, Reducer, combiner_class=Reducer))
