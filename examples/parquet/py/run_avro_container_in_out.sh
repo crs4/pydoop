bba99b03a81895d52eccee24c4c7670ab2664b1a
simleo <simleo@crs4.it>
2015-03-03 17:39:51 +0100

    added support for avro container output keys

--- examples/parquet/py/run_avro_container_in_out.sh
+++ examples/parquet/py/run_avro_container_in_out.sh
@@ -7,6 +7,13 @@ die() {
     exit 1
 }
 
+USER_SCHEMA_FN=../../avro/schemas/user.avsc
+STATS_SCHEMA_FN=../../avro/schemas/stats.avsc
+STATS_SCHEMA=`cat ${STATS_SCHEMA_FN}`
+CSV_FN=users.csv
+AVRO_FN=users.avro
+OUTPUT=results
+
 nargs=1
 if [ $# -ne $nargs ]; then
     die "Usage: $0 k|v|kv"
@@ -15,10 +22,12 @@ mode=$1
 if [ "${mode}" == "k" ]; then
     INPUT_FORMAT=it.crs4.pydoop.mapreduce.pipes.PydoopAvroKeyInputFormat
     OUTPUT_FORMAT=it.crs4.pydoop.mapreduce.pipes.PydoopAvroKeyOutputFormat
+    OUT_SCHEMA_PROP=pydoop.mapreduce.avro.key.output.schema
     MODULE=avro_key_in_out
 elif [ "${mode}" == "v" ]; then
     INPUT_FORMAT=it.crs4.pydoop.mapreduce.pipes.PydoopAvroValueInputFormat
     OUTPUT_FORMAT=it.crs4.pydoop.mapreduce.pipes.PydoopAvroValueOutputFormat
+    OUT_SCHEMA_PROP=pydoop.mapreduce.avro.value.output.schema
     MODULE=avro_value_in_out
 elif [ "${mode}" == "kv" ]; then
     # FIXME: not supported yet
@@ -29,16 +38,11 @@ else
     die "invalid mode: ${mode}"
 fi
 
-IN_SCHEMA_FILE_LOCAL=../../avro/schemas/user.avsc
-OUT_SCHEMA_FILE_LOCAL=../../avro/schemas/stats.avsc
-CSV_FN=users.csv
-AVRO_FN=users.avro
-OUTPUT=results
 
 # --- generate avro input ---
 N=20
 python ../java/create_input.py ${N} ${CSV_FN}
-python write_avro.py ${IN_SCHEMA_FILE_LOCAL} ${CSV_FN} ${AVRO_FN}
+python write_avro.py ${USER_SCHEMA_FN} ${CSV_FN} ${AVRO_FN}
 
 hdfs dfs -mkdir -p /user/${USER}
 hdfs dfs -rm ${AVRO_FN}
@@ -49,14 +53,13 @@ MPY=${MODULE}.py
 JOBNAME=${MODULE}-job
 LOGLEVEL=DEBUG
 MRV="--mrv2"
-STATS_SCHEMA=`cat ${OUT_SCHEMA_FILE_LOCAL}`
 
 INPUT=${AVRO_FN}
 
 hdfs dfs -rmr /user/${USER}/${OUTPUT}
 
 pydoop submit \
-    -D pydoop.mapreduce.avro.value.output.schema="${STATS_SCHEMA}" \
+    -D "${OUT_SCHEMA_PROP}"="${STATS_SCHEMA}" \
     --upload-file-to-cache avro_base.py \
     --upload-file-to-cache ${MPY} \
     --num-reducers 1 \
