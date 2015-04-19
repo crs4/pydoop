5bf8369ec0802ecf43bebdab23321208a545133d
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2015-04-19 23:30:24 +0200

    Improved yarn configuration.
    We are still missing mr log files, though...

--- dev_tools/docker/clusters/apache_2.6.0/images/nodemanager/scripts/start_nodemanager.sh
+++ dev_tools/docker/clusters/apache_2.6.0/images/nodemanager/scripts/start_nodemanager.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+export YARN_LOG_DIR=${YARN_LOG_DIR}
+export HADOOP_PID_DIR=${HDFS_PID_DIR}
+
 python /tmp/zk_wait.py nodemanager
+
+# YARN_OPTS="$YARN_OPTS -Dhadoop.log.dir=$YARN_LOG_DIR"
+# YARN_OPTS="$YARN_OPTS -Dyarn.log.dir=$YARN_LOG_DIR"
+# YARN_OPTS="$YARN_OPTS -Dhadoop.log.file=$YARN_LOGFILE"
+# YARN_OPTS="$YARN_OPTS -Dyarn.log.file=$YARN_LOGFILE"
+# YARN_OPTS="$YARN_OPTS -Dyarn.home.dir=$YARN_COMMON_HOME"
+# YARN_OPTS="$YARN_OPTS -Dyarn.id.str=$YARN_IDENT_STRING"
+# YARN_OPTS="$YARN_OPTS -Dhadoop.root.logger=${YARN_ROOT_LOGGER:-INFO,console}"
+# YARN_OPTS="$YARN_OPTS -Dyarn.root.logger=${YARN_ROOT_LOGGER:-INFO,console}"
+
+
+su - ${YARN_USER} -p -c "${HADOOP_HOME}/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start nodemanager"
 
 # we should actually check that the nodemanager is up ...
 python /tmp/zk_set.py nodemanager up
 
-su ${YARN_USER} -c "${HADOOP_HOME}/bin/yarn --config ${HADOOP_CONF_DIR} nodemanager 2>&1 >/tmp/logs/nodemanager.out"
+echo log is ${YARN_LOG_DIR}/*nodemanager-${HOSTNAME}.out
+
+tail -f ${YARN_LOG_DIR}/*nodemanager-${HOSTNAME}.out
+
--- dev_tools/docker/clusters/apache_2.6.0/images/resourcemanager/scripts/start_resourcemanager.sh
+++ dev_tools/docker/clusters/apache_2.6.0/images/resourcemanager/scripts/start_resourcemanager.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-export HADOOP_LOG_DIR=${HDFS_LOG_DIR}
+export YARN_LOG_DIR=${YARN_LOG_DIR}
 export HADOOP_PID_DIR=${HDFS_PID_DIR}
+export YARN_OPTS=''
+
+# YARN_OPTS="$YARN_OPTS -Dhadoop.log.dir=$YARN_LOG_DIR"
+# YARN_OPTS="$YARN_OPTS -Dyarn.log.dir=$YARN_LOG_DIR"
+# YARN_OPTS="$YARN_OPTS -Dhadoop.log.file=$YARN_LOGFILE"
+# YARN_OPTS="$YARN_OPTS -Dyarn.log.file=$YARN_LOGFILE"
+# YARN_OPTS="$YARN_OPTS -Dyarn.home.dir=$YARN_COMMON_HOME"
+# YARN_OPTS="$YARN_OPTS -Dyarn.id.str=$YARN_IDENT_STRING"
+# YARN_OPTS="$YARN_OPTS -Dhadoop.root.logger=${YARN_ROOT_LOGGER:-INFO,console}"
+# YARN_OPTS="$YARN_OPTS -Dyarn.root.logger=${YARN_ROOT_LOGGER:-INFO,console}"
 
 
 python /tmp/zk_wait.py resourcemanager
 
+su - ${YARN_USER} -p -c "${HADOOP_HOME}/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager"
+
 # we should actually check that the resourcemanager is up ...
 python /tmp/zk_set.py resourcemanager up
 
+echo log is ${YARN_LOG_DIR}/*resourcemanager-${HOSTNAME}.out
 
-su ${YARN_USER} -c "${HADOOP_HOME}/bin/yarn --config ${HADOOP_CONF_DIR} resourcemanager 2>&1 > /tmp/logs/resourcemanager.out"
+tail -f ${YARN_LOG_DIR}/*resourcemanager-${HOSTNAME}.out
 
