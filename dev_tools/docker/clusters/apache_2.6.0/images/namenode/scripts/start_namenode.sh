899bfb00acaa9431988e7c3e13e2f2284b9772ec
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2015-04-19 23:12:07 +0200

    Wait a little bit more to declare namenode as started.

--- dev_tools/docker/clusters/apache_2.6.0/images/namenode/scripts/start_namenode.sh
+++ dev_tools/docker/clusters/apache_2.6.0/images/namenode/scripts/start_namenode.sh
@@ -8,9 +8,6 @@ python /tmp/zk_wait.py namenode
 
 su ${HDFS_USER} -c "${HADOOP_HOME}/bin/hdfs --config ${HADOOP_CONF_DIR} namenode -format"
 
-# we should actually check that the namenode is up ...
-python /tmp/zk_set.py namenode up
-
 su - ${HDFS_USER} -p -c "${HADOOP_HOME}/sbin/hadoop-daemon.sh --config ${HADOOP_CONF_DIR} start namenode"
 
 # we should actually check that the namenode is up ...
