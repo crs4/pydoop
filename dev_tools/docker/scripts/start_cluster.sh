53c43dd377a742541e940ef345ac579059ab99a9
kikkomep <kikkomep@crs4.it>
2015-04-21 17:06:03 +0200

    fix expected parameter (i.e., cluster_dir_name) and correctly compute the cluster_dir path

--- dev_tools/docker/scripts/start_cluster.sh
+++ dev_tools/docker/scripts/start_cluster.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
+cluster_name=$1
 real_path=`realpath $0`
-cluster_path=$1
-
-#----------------
 script_dir=`dirname ${real_path}`
 share_hosts_bin="python ${script_dir}/share_etc_hosts.py"
+cluster_path="${script_dir}/../clusters/${cluster_name}"
 
-cluster_dir=`basename ${cluster_path}`
-tag=`echo ${cluster_dir} | tr -d '._/'`
-
+tag=`echo ${cluster_name} | tr -d '._/'`
 
 cd ${cluster_path}
 docker-compose up -d
