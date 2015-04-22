1bc7360bea4bd996c806a72b16db26bc0cb1d552
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2015-04-22 11:02:33 +0200

    start_cluster.sh now tries to clean-up first.

--- dev_tools/docker/scripts/start_cluster.sh
+++ dev_tools/docker/scripts/start_cluster.sh
@@ -9,5 +9,20 @@ cluster_path="${script_dir}/../clusters/${cluster_name}"
 tag=`echo ${cluster_name} | tr -d '._/'`
 
 cd ${cluster_path}
+
+docker-compose stop
+docker-compose rm
+
+for x in logs local
+do
+    if [ -d ${x} ]; then
+        backup=${x}.backup.$$
+        mv ${x} ${backup}
+        echo "Moved ${x} to ${backup}"
+    fi
+    mkdir ${x}
+    chmod 1777 ${x}
+done
+
 docker-compose up -d
 ${share_hosts_bin} ${tag}
