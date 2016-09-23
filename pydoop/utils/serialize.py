17e0675439f6af93cc424ced1ca0428ce36bcd6d
Gianluigi Zanetti <zag@crs4.it>
2016-09-23 14:59:13 +0200

    Put back pure python serialization functions.
    The are still needed by mapreduce.pipes.

--- pydoop/utils/serialize.py
+++ pydoop/utils/serialize.py
@@ -72,6 +72,7 @@ The idea is to mimick Hadoop writable interface, so that we can then write:
                     codec.serialize_object(value))
 """
 import struct
+import xdrlib
 
 from .py3compat import pickle, unicode
 
@@ -92,11 +93,120 @@ def private_decode(s):
     return pickle.loads(s)
 
 
+# The following is a reimplementation of the Hadoop Pipes c++ utils functions.
+# Do not use these functions in time-critical regions.
+
+
 def read_buffer(n, stream):
     buff = stream.read(n)
     if len(buff) != n:
         raise EOFError
     return buff
+
+
+def serialize_int(t, stream):
+    p = xdrlib.Packer()
+    p.pack_int(t)
+    stream.write(p.get_buffer())
+
+
+def deserialize_int(stream):
+    SIZE_OF_INT = 4
+    buf = read_buffer(SIZE_OF_INT, stream)
+    up = xdrlib.Unpacker(buf)
+    return up.unpack_int()
+
+
+def deserialize_long(stream):
+    SIZE_OF_LONG = 8
+    buf = read_buffer(SIZE_OF_LONG, stream)
+    return struct.unpack('>q', buf)[0]
+
+
+def serialize_long(v, stream):
+    stream.write(struct.pack('>q', v))
+
+
+def serialize_vint(t, stream):
+    if -112 <= t <= 127:
+        stream.write(struct.pack('b', t))
+    else:
+        l = -112
+        if t < 0:
+            t ^= -1  # remove sign
+            l = -120  # encode negativeness in l
+        size = (t.bit_length() + 7) // 8
+        stream.write(struct.pack('b', l - size))
+        # we assume that integers are at most long long
+        stream.write(struct.pack('>Q', t)[-size:])
+    return
+
+
+def deserialize_vint(stream):
+    b = struct.unpack('b', read_buffer(1, stream))[0]
+    if b >= -112:
+        return b
+    (negative, l) = (True, -120 - b) if b < -120 else (False, -112 - b)
+    q = struct.unpack('>Q', b'\x00' * (8 - l) + read_buffer(l, stream))[0]
+    return q ^ -1 if negative else q
+
+
+def serialize_float(t, stream):
+    p = xdrlib.Packer()
+    p.pack_float(t)
+    stream.write(p.get_buffer())
+
+
+def deserialize_float(stream):
+    """
+    **NOTE:** floats are serialized using XDR (4 bytes).  When mapped
+    back to python, the float is upgraded to a double, so it could be
+    slightly different than the original value.
+    """
+    SIZE_OF_FLOAT = 4
+    buf = read_buffer(SIZE_OF_FLOAT, stream)
+    up = xdrlib.Unpacker(buf)
+    return up.unpack_float()
+
+
+def serialize_bool(v, stream):
+    serialize_vint(int(v), stream)
+
+
+def deserialize_bool(stream):
+    return bool(deserialize_vint(stream))
+
+
+def serialize_bytes(s, stream):
+    serialize_vint(len(s), stream)
+    if len(s) > 0:
+        stream.write(s)
+
+
+def deserialize_bytes(stream):
+    """
+    This is the wire format generically used by hadoop pipes.
+    The data length is encoded using VInt.  Note that Text is encoded in UTF-8
+    (so use deserialize_text) and complex objects can implement their own
+    serialization -- within the (data length, data) stream structure.
+    """
+    l = deserialize_vint(stream)
+    return read_buffer(l, stream)
+
+
+def serialize_text(s, stream):
+    if isinstance(s, unicode):
+        data = s.encode('UTF-8')
+    else:
+        data = s
+    serialize_vint(len(data), stream)
+    if len(data) > 0:
+        stream.write(data)
+
+
+def deserialize_text(stream):
+    l = deserialize_vint(stream)
+    return unicode(read_buffer(l, stream), 'UTF-8')
 
 
 def serialize_old_style_filename(s, stream):
