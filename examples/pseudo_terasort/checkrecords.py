e61ce7b5a3b97c5b4596572bc113985c23e099b1
simleo <simleo@crs4.it>
2017-12-06 11:14:04 +0100

    pep8/copyright/cosmetic fixes

--- examples/pseudo_terasort/checkrecords.py
+++ examples/pseudo_terasort/checkrecords.py
@@ -1,12 +1,30 @@
+# BEGIN_COPYRIGHT
+#
+# Copyright 2009-2017 CRS4.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# END_COPYRIGHT
+
+import sys
+import os
+import argparse
+
 from pydoop.app.submit import (
     add_parser_common_arguments,
     add_parser_arguments)
 from pydoop.app.submit import PydoopSubmitter
 import pydoop.hdfs as hdfs
-
-import sys
-import os
-import argparse
 
 
 def make_parser():
@@ -46,4 +64,5 @@ def main(argv=None):
     check_rows(data.split(b'\n')[:-1])
 
 
-main(sys.argv)
+if __name__ == "__main__":
+    main(sys.argv)
