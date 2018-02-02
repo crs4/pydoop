0599fdf621a0f3ce7141167ea7d7f6a948ca7df4
simleo <simleo@crs4.it>
2018-02-02 12:35:04 +0100

    RangeInputFormat: fixed copyright; cosmetic fixes

--- examples/pseudo_terasort/it/crs4/pydoop/examples/pterasort/RangeInputSplit.java
+++ examples/pseudo_terasort/it/crs4/pydoop/examples/pterasort/RangeInputSplit.java
@@ -1,80 +1,78 @@
-// BEGIN_COPYRIGHT
-// 
-// Copyright 2009-2016 CRS4.
-// 
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not
-// use this file except in compliance with the License. You may obtain a copy
-// of the License at
-// 
-//   http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-// 
-// END_COPYRIGHT
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package it.crs4.pydoop.examples.pterasort;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableUtils;
-// import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 
-
 /**
- * An input split consisting of a range on numbers.
+ * An input split consisting of a range of numbers.
+ * This mimics the original RangeInputSplit, but we have to extend
+ * FileSplit to make pipes happy.
  */
-//public class RangeInputSplit extends InputSplit implements Writable {
-// FIXME: InputSplit is not Writable, see it.crs4.mapreduce.pipes.PipesMapper
-// FIXME: still, it is ugly.
 public class RangeInputSplit extends FileSplit {
-    long firstRow;
-    long rowCount;
 
-    public RangeInputSplit() { }
+  long firstRow;
+  long rowCount;
 
-    public RangeInputSplit(long offset, long length) {
-        firstRow = offset;
-        rowCount = length;
-    }
+  public RangeInputSplit() {}
 
-    /** The file containing this split's data. */
-    @Override
-    // FIXME: HACK!
-    public Path getPath() { return new Path("file:///dev/null"); } 
-  
-    /** The position of the first byte in the 'file' to process. */
-    @Override    
-    public long getStart() { return 0; }
-  
-    /** The number of bytes in the 'file' to process. */
-    @Override
-    public long getLength() /* throws IOException */ {
-        return 0;
-    }
+  public RangeInputSplit(long offset, long length) {
+    firstRow = offset;
+    rowCount = length;
+  }
 
-    @Override
-    public String[] getLocations() /* throws IOException */ {
-        return new String[]{};
-    }
+  @Override
+  public Path getPath() {
+    return new Path("file:///dev/null");  // not a real FileSplit
+  }
 
-    @Override
-    public void readFields(DataInput in) throws IOException {
-        firstRow = WritableUtils.readVLong(in);
-        rowCount = WritableUtils.readVLong(in);
-    }
+  @Override
+  public long getStart() {
+    return 0;
+  }
 
-    @Override
-    public void write(DataOutput out) throws IOException {
-        WritableUtils.writeVLong(out, firstRow);
-        WritableUtils.writeVLong(out, rowCount);
-    }
+  @Override
+  public long getLength() {
+    return 0;
+  }
+
+  @Override
+  public String[] getLocations() {
+    return new String[]{};
+  }
+
+  @Override
+  public void readFields(DataInput in) throws IOException {
+    firstRow = WritableUtils.readVLong(in);
+    rowCount = WritableUtils.readVLong(in);
+  }
+
+  @Override
+  public void write(DataOutput out) throws IOException {
+    WritableUtils.writeVLong(out, firstRow);
+    WritableUtils.writeVLong(out, rowCount);
+  }
+
 }
