4b2927f1a5ba143593b1eb6fd947282e090c7de4
Gianluigi Zanetti <zag@crs4.it>
2018-03-09 23:24:06 +0100

    Added missing imports and minor cleanup.

--- test/src/it/crs4/pydoop/mapreduce/pipes/TestPipesExternalSplits.java
+++ test/src/it/crs4/pydoop/mapreduce/pipes/TestPipesExternalSplits.java
@@ -24,8 +24,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.JobID;
+import org.apache.hadoop.mapreduce.TaskAttemptID;
+import org.apache.hadoop.mapreduce.TaskID;
+import org.apache.hadoop.mapreduce.TaskType;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl;
+import org.apache.hadoop.io.IntWritable;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.StringUtils;
@@ -44,39 +55,51 @@ public class TestPipesExternalSplits {
 
   @Test
   public void testExternalSplitsSupport() throws IOException, InterruptedException {
-      Configuration conf = new Configuration();
+      JobID jobId = new JobID("201408272347", 0);
+      TaskID taskId = new TaskID(jobId, TaskType.MAP, 0);
+      TaskAttemptID taskAttemptid = new TaskAttemptID(taskId, 0);
+
+      Job job = new Job(new Configuration());
+      job.setJobID(jobId);
+      Configuration conf = job.getConfiguration();
       // FIXME this is hardwired. Use tmpfile or something like that.
-      final String uri = new String("file:///tmp/a_binary_file.bin");
+      final String uri = new String("a_binary_file.bin");
       conf.setBoolean(PipesNonJavaInputFormat.EXTERNAL_SPLITS_ENABLED, true);
-      conf.setBoolean(PipesNonJavaInputFormat.EXTERNAL_SPLITS_URI, uri);
+      conf.set(PipesNonJavaInputFormat.EXTERNAL_SPLITS_URI, uri);
 
+      TaskAttemptContextImpl tcontext = 
+          new TaskAttemptContextImpl(conf, taskAttemptid);
+      
       PipesNonJavaInputFormat input_format = new PipesNonJavaInputFormat();
-      List<InputSplit> written = write_input_splits(conf, uri);
-      List<InputSplit> read = input_format.getSplits(conf);
+      int n_splits = 10;
+      List<InputSplit> written = write_input_splits(n_splits, conf, uri);
+      List<InputSplit> read = input_format.getSplits(tcontext);
 
-      assertEquals(written.size(), read.size());
+      assertEquals(written.size(), n_splits);
+      assertEquals(written.size(), read.size());      
       for(int i = 0; i < read.size(); i++) {
-          OpaqueSplit itwas = (OpaqueSplit) written[i];
-          OpaqueSplit itis = (OpaqueSplit) read[i];
-          assertEquals(itwas.code, itis.code);
-          assertEquals(itwas.payload, itis.payload);          
+          OpaqueSplit itwas = (OpaqueSplit) written.get(i);
+          OpaqueSplit itis = (OpaqueSplit) read.get(i);
+          assertEquals(itwas.getCode(), itis.getCode());
+          assertEquals(itwas.getPayload(), itis.getPayload());          
       }
   }
 
-  private List<InputSplit> write_input_splits(Configuration conf,
-                                              String uri)
-                                              throws IOException,
+    private List<InputSplit> write_input_splits(int n_splits,
+                                                Configuration conf,
+                                                String uri)
+                                               throws IOException,
                                                      InterruptedException {  
       FileSystem fs = FileSystem.get(conf);
       Path path = new Path(uri);
-      FSDataOutputStream out = fs.open(path, "w");
-      IntWritable n_records = new IntWritable(10);
+      FSDataOutputStream out = fs.create(path);
+      IntWritable n_records = new IntWritable(n_splits);
       n_records.write(out);
       List<InputSplit> splits = new ArrayList<InputSplit>();
       
       for(int i = 0; i < n_records.get(); i++) {
           String code = "code-" + i;
-          String code = "payload-" + i;
+          String payload = "payload-" + i;
           OpaqueSplit osplit = new OpaqueSplit(code.getBytes(), payload.getBytes());
           osplit.write(out);
           splits.add(osplit);
@@ -85,7 +108,5 @@ public class TestPipesExternalSplits {
       out.close();
       fs.close();
       return splits;
-    }
-    
   }
 }
