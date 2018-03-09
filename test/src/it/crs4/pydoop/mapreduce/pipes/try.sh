4dfd33c5dfce194a1a6b4ed97e2c3f0422e51ef1
Gianluigi Zanetti <zag@crs4.it>
2018-03-09 19:24:35 +0100

    Clean up of unused tests.

--- test/src/it/crs4/pydoop/mapreduce/pipes/try.sh
+++ test/src/it/crs4/pydoop/mapreduce/pipes/try.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
-export HADOOP_PREFIX=/opt/hadoop
-export PATH=${HADOOP_PREFIX}/bin:${PATH}
 
-PYDRIVER=/home/zag/work/vs/git/pydoop/src/it/crs4/pydoop/pypipes_classes
+PYDOOP_CP=`pydoop classpath`
+HADOOP_CP=`hadoop classpath`
+CP="${PYDOOP_CP}:${HADOOP_CP}"
+
+SRC="TestPipesNonJavaInputFormat.java"
+TESTED_CLASSES="TestPipesExternalSplits"
+
+rm -rf test_classes
+javac -cp ${CP} ${SRC} -d test_classes
 
 
-SRC="TestPipeApplication.java TestPipesNonJavaInputFormat.java CommonStub.java PipeApplicationStub.java PipeApplicationRunnableStub.java DummyInputFormat.java PipeReducerStub.java"
-javac -cp `hadoop classpath`:${PYDRIVER} ${SRC} -d test_classes
-
-for class in TestPipeApplication TestPipesNonJavaInputFormat TestPipesExternalSplits
+for class in ${TESTED_CLASSES}
 do 
     echo "Testing ${class}"
-    java -cp $PWD/test_classes:`hadoop classpath`:${PYDRIVER} org.junit.runner.JUnitCore it.crs4.pydoop.pipes.${class}
+    java -cp $PWD/test_classes:${CP} \
+         org.junit.runner.JUnitCore it.crs4.pydoop.pipes.mapreduce.${class}
 done
-
-exit
-rm pypipes.jar; jar -cvf pypipes.jar -C pypipes_classes/ .
-
-hadoop jar pypipes.jar it.crs4.pydoop.pipes.Submitter -libjars pypipes.jar \
-           -D hadoop.pipes.java.recordreader=true -D hadoop.pipes.java.recordwriter=true \
-           -inputformat it.crs4.pydoop.input.PydoopInputFormat \
-           -program user_counts.py -reduces 0 -input input -output output
-
