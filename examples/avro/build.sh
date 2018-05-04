474f6d4e00ff349dbd289aa67d05d9ef40688b16
simleo <simleo@crs4.it>
2018-05-04 15:47:32 +0200

    avro examples: fixed build; moved runners to top level

--- examples/avro/build.sh
+++ examples/avro/build.sh
@@ -5,23 +5,12 @@ set -euo pipefail
 this="${BASH_SOURCE-$0}"
 this_dir=$(cd -P -- "$(dirname -- "${this}")" && pwd -P)
 . "${this_dir}/../config.sh"
-
-nargs=2
-if [ $# -ne ${nargs} ]; then
-    die "Usage: $0 CP_PATH JAR_PATH"
-fi
-CP_PATH=$1
-JAR_PATH=$2
-
-WD=$(mktemp -d)
-CLASS_DIR="${WD}"/classes
-mkdir -p "${CLASS_DIR}"
+. "${this_dir}/config.sh"
 
 pushd "${this_dir}"
-./dep_classpath "${CP_PATH}"
+gen_classpath
 cp="$(<"${CP_PATH}"):$(${HADOOP} classpath)"
-javac -cp "${cp}" -d "${CLASS_DIR}" java/src/main/java/it/crs4/pydoop/*
+mkdir -p "${CLASS_DIR}"
+javac -cp "${cp}" -d "${CLASS_DIR}" src/main/java/it/crs4/pydoop/*
 jar -cf "${JAR_PATH}" -C "${CLASS_DIR}" ./it
 popd
-
-rm -rf "${WD}"
