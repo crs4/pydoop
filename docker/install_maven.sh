dfae313d6270200b4d1c156d8d87fa826b3eed39
simleo <simleo@crs4.it>
2019-02-06 11:21:30 +0100

    install_maven: get latest version from ftp ls

--- docker/install_maven.sh
+++ docker/install_maven.sh
@@ -2,17 +2,18 @@
 
 set -euo pipefail
 
-VERSION="3.5.3"
-TAR="apache-maven-${VERSION}-bin.tar.gz"
-REPO="ftp://ftp.mirrorservice.org/sites/ftp.apache.org/maven/maven-3"
-MVN_HOME="/opt/apache-maven"
+home="/opt/apache-maven"
+repo="ftp://ftp.mirrorservice.org/sites/ftp.apache.org/maven/maven-3"
 
-curl -o ${TAR} ${REPO}/${VERSION}/binaries/${TAR}
-tar xf ${TAR}
-mv apache-maven-${VERSION} ${MVN_HOME}
+version=$(curl -l "${repo}"/ | sort | tail -n 1)
+tar="apache-maven-${version}-bin.tar.gz"
+
+curl -o ${tar} ${repo}/${version}/binaries/${tar}
+tar xf ${tar}
+mv apache-maven-${version} ${home}
 
 cat >/etc/profile.d/maven.sh <<EOF
-export M2_HOME="${MVN_HOME}"
+export M2_HOME="${home}"
 export M2="\${M2_HOME}/bin"
 export PATH="\${M2}:\${PATH}"
 EOF
