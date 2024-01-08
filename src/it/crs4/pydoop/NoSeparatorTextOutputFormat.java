5e62a05f249418d564ccda3137a6400f40399137
simleo <simleo@crs4.it>
2024-01-08 14:11:24 +0100

    update copyright end year [ci skip]

--- src/it/crs4/pydoop/NoSeparatorTextOutputFormat.java
+++ src/it/crs4/pydoop/NoSeparatorTextOutputFormat.java
@@ -1,7 +1,7 @@
 
 // BEGIN_COPYRIGHT
 // 
-// Copyright 2009-2022 CRS4.
+// Copyright 2009-2024 CRS4.
 // 
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy
